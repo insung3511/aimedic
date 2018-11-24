f877f3f965ec109cf64917b276998ad4c3367303
박인성 (Bahk InSung) <insung.park123@gmail.com>
2018-11-24 16:48:33 +0900

    4214 Drug Photos
    4214 약 사진

---
+++ Yakjaengii-Project/Photos/4214/take-pic.py
@@ -0,0 +1,17 @@
+from picamera import PiCamera
+from time import sleep
+from PIL import Image
+
+camera = PiCamera()
+mydir  = '/home/pi/Yakjaengii-Project/Photos/4214'
+
+camera.start_preview()
+for i in range(20):
+	filename = 'ato' + str(i) + '.jpg'
+	print(filename)
+	fullpath = mydir + filename
+	camera.capture(fullpath)
+	imageObj = Image.open(fullpath)
+	cropped  = imageObj.crop((190,80,590,380))
+	cropped.save("cropped_" + filename)
+camera.stop_preview()
